32c1f21ae82e331663e5cea9f97e73316b98ff6a
Silas Rodrigues <79548192+SilasRSA@users.noreply.github.com>
2022-01-30 14:02:22 -0300

    Frist program
    Meu primeiro programa em Python

---
+++ first_program.py
@@ -0,0 +1,11 @@
+# My first lab in Python
+
+# Try fisrt Python output
+print('Hello,Python')
+
+# Practice on writing comments
+print('What is Python version?') # This line prints a string
+
+# Check the Python version
+import sys
+print(sys.version)  
